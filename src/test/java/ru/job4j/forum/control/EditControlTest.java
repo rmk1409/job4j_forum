6bfaf8356babe8f46ba23c4819bcf80eae34b96e
Roman Pogorelov <rmk140987@gmail.com>
2021-09-28 15:09:40 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/forum/control/EditControlTest.java
+++ src/test/java/ru/job4j/forum/control/EditControlTest.java
@@ -1,14 +1,23 @@
 package ru.job4j.forum.control;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -18,13 +27,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class EditControlTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private PostService posts;
 
     @Test
     @WithMockUser
-    public void shouldReturnDefaultMessage() throws Exception {
+    public void getRequest() throws Exception {
         this.mockMvc.perform(get("/edit"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("edit"));
+    }
+
+    @Test
+    @WithMockUser
+    public void save() throws Exception {
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("name", "some-cool-name.");
+        params.add("desc", "some-cool-descr.");
+        this.mockMvc
+                .perform(post("/edit")
+                        .params(params))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(posts).save(argument.capture());
+        MatcherAssert.assertThat(argument.getValue().getName(), is("some-cool-name."));
+        MatcherAssert.assertThat(argument.getValue().getDesc(), is("some-cool-descr."));
     }
 }
--- src/test/java/ru/job4j/forum/control/RegControlTest.java
+++ src/test/java/ru/job4j/forum/control/RegControlTest.java
@@ -1,14 +1,26 @@
 package ru.job4j.forum.control;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.User;
+import ru.job4j.forum.service.UserService;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -18,6 +30,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class RegControlTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private UserService users;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     @WithMockUser
@@ -26,5 +42,23 @@ class RegControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("reg"));
+    }
+
+    @Test
+    @WithMockUser
+    public void save() throws Exception {
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("username", "some-cool-name.");
+        params.add("password", "some-cool-password.");
+        when(users.add(any())).thenReturn(true);
+        this.mockMvc
+                .perform(post("/reg")
+                        .params(params))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+        verify(users).add(argument.capture());
+        MatcherAssert.assertThat(argument.getValue().getUsername(), is("some-cool-name."));
+        MatcherAssert.assertThat(true, is(passwordEncoder.matches("some-cool-password.", passwordEncoder.encode("some-cool-password."))));
     }
 }
